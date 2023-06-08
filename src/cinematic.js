629f83aed2e5ff6b9e9bb430cc9f913c652f4f09
Ethan <ethankeyser05@gmail.com>
2023-06-08 16:14:04 -0700

    worked more on prototype

--- src/cinematic.js
+++ src/cinematic.js
@@ -26,10 +26,14 @@ class Menu extends Phaser.Scene{
     }
     create(){
         this.title = this.add.text(-500, 50, "Game Title").setFontSize(100);
-        this.start = this.add.text(-300, 500, "Start").setFontSize(50);
+        this.start = this.add.text(-300, 500, "Start", {color: '#ffffff'}).setFontSize(50);
+        this.start.setInteractive();
         this.resume = this.add.text(-375, 575, "Resume").setFontSize(50);
+        this.resume.setInteractive();
         this.options = this.add.text(-450, 650, "Options").setFontSize(50);
+        this.options.setInteractive();
         this.exit = this.add.text(-525, 725, "Exit").setFontSize(50);
+        this.exit.setInteractive();
 
         this.tweens.add({
             targets: this.title,
@@ -69,6 +73,61 @@ class Menu extends Phaser.Scene{
                 ease: 'Linear',
             })
         })
+
+        this.start.on('pointerover', () =>{
+            this.resume.setColor('#ffffff');
+            this.start.setColor('#0000ff');
+            this.options.setColor('#ffffff');
+            this.exit.setColor('#ffffff');
+        })
+        this.resume.on('pointerover', () => {
+            this.resume.setColor('#0000ff');
+            this.start.setColor('#ffffff');
+            this.options.setColor('#ffffff');
+            this.exit.setColor('#ffffff');
+        })
+        this.options.on('pointerover', () => {
+            this.resume.setColor('#ffffff');
+            this.start.setColor('#ffffff');
+            this.options.setColor('#0000ff');
+            this.exit.setColor('#ffffff');
+        })
+        this.exit.on('pointerover', () => {
+            this.resume.setColor('#ffffff');
+            this.start.setColor('#ffffff');
+            this.options.setColor('#ffffff');
+            this.exit.setColor('#0000ff');
+        })
+        this.start.on('pointerdown', () => {
+            this.cameras.main.fadeOut(1000,0,0,0);
+            this.camersa.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) =>{
+                this.scene.start('scene1');
+            })
+        })
+    }
+}
+
+class Scene1 extends Phaser.Scene {
+    constructor(){
+        super('scene1');
+    }
+    preload(){
+
+    }
+    create(){
+
+    }
+}
+
+class Scene2 extends Phaser.Scene {
+    constructor(){
+        super('scene2');
+    }
+    preload(){
+
+    }
+    create(){
+
     }
 }
 
@@ -81,7 +140,7 @@ let config = {
         height: 1080
     },
     backgroundColor: '#301934',
-    scene: [ Logo, Menu],
+    scene: [ Logo, Menu, Scene1, Scene2],
 }
 
 let game = new Phaser.Game(config);
