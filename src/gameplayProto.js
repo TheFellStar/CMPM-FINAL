1af9ebba157ca5b6994eab942c2ad31232f4127c
Ethan <ethankeyser05@gmail.com>
2023-06-11 14:31:35 -0700

    added present assets

--- src/gameplayProto.js
+++ src/gameplayProto.js
@@ -12,9 +12,20 @@ class Level1 extends Phaser.Scene {
         this.load.image('door', 'door_present.png');
         this.load.image('lock', 'lock_present.png');
         this.load.image('tv', 'tv_present.png');
+        this.load.image('table', 'table_present.png');
+        this.load.image('player', 'player.png');
     }
     create(){
-
+        this.bookshelf = this.add.image(1400, 150, 'bookshelf').setScale(.3);
+        this.diningTable = this.add.image(500, 300, 'table').setScale(.35);
+        this.carpet = this.add.image(700, 600, 'carpet').setScale(.3);
+        this.table = this.add.image(500, 1000, 'table').setScale(.35);
+        this.sofa = this.add.image(900, 600, 'couch').setScale(.3);
+        this.sofa.angle = 90;
+        this.tv = this.add.image(100, 600, 'tv').setScale(.3);
+        this.chair = this.add.image(1150, 950, 'chair').setScale(.3);
+        this.door = this.add.image(1800, 500, 'door').setScale(.3);
+        this.player = this.add.image(1400, 600, 'player').setScale(.3);
     }
 }
 
@@ -24,6 +35,62 @@ class Level1Alt extends Phaser.Scene {
     }
     create(){
 
+    }
+}
+
+class Pause extends Phaser.Scene {
+    constructor(){
+        super('pause');
+    }
+    create(){
+        this.add.text(800, 50, "Game Paused").setFontSize(50);
+        this.cont = this.add.text(900, 500, "Continue").setFontSize(30);
+        this.cont.setInteractive();
+        this.check = this.add.text(900, 600, "Check Clues").setFontSize(30);
+        this.check.setInteractive();
+        this.options = this.add.text(900, 700, "Options").setFontSize(30);
+        this.options.setInteractive();
+        this.exit = this.add.text(900, 800, "Exit").setFontSize(30);
+        this.exit.setInteractive();
+
+        this.cont.on('pointerdown', () => {
+            if(paused == 1){
+                this.scene.start('level1');
+            }
+            if(paused == 2){
+                this.scene.start('level1alt');
+            }
+        })
+        this.check.on('pointerdown', () => {
+            this.scene.start('clues');
+        })
+        this.options.on('pointerdown', () => {
+            this.scene.start('options');
+        })
+    }
+}
+
+class Clues extends Phaser.Scene {
+    constructor(){
+        super('clues');
+    }
+    create(){
+        this.add.text(800, 500, "Clues will appear here").setFontSize(50);
+        this.input.on('pointerdown', () => {
+            this.scene.start('pause');
+        })
+    }
+}
+
+class Options extends Phaser.Scene {
+    constructor(){
+        super('options');
+    }
+    create(){
+        this.add.text(800, 500, "Options will go here").setFontSize(50);
+        this.input.on('pointerdown', () => {
+            this.scene.start('pause');
+        })
     }
 }
 
@@ -39,7 +106,7 @@ let config = {
         default: "arcade",
     },
     backgroundColor: '#301934',
-    scene: [ Level1, Level1Alt],
+    scene: [ Level1, Level1Alt, Pause, Clues, Options],
 }
 
 let game = new Phaser.Game(config);
